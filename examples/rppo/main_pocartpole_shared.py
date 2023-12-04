9e8559f592f386515f699756e7f7db304f747b37
Seladus <clement.blancovolle@outlook.fr>
2023-12-04 21:10:22 +0100

    adding  example on pocartpole for shared layers networks

--- examples/rppo/main_pocartpole_shared.py
+++ examples/rppo/main_pocartpole_shared.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     np.random.seed(seed)
     torch.random.manual_seed(seed)
 
-    config = Config("./examples/rppo/pocartpole_config.yml")
+    config = Config("./examples/rppo/pocartpole_config_shared.yml")
     env = gym.vector.make("POCartPole-v1", asynchronous=True, num_envs=config.n_envs)
     test_env = gym.vector.make(
         "POCartPole-v1", asynchronous=False, num_envs=1, max_episode_steps=500
@@ -33,7 +33,6 @@ if __name__ == "__main__":
     )
     env = RecordEpisodeStatistics(env)
     env = NormalizeObservation(env)
-    env = NormalizeReward(env)
     env.reset(seed=seed)
     agent = SharedRecurrentAgent(
         obs_dim, action_dim, f_hidden=[], f_enc=[64], f_actor=[64], f_critic=[64]
